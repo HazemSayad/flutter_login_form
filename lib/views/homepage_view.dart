61992f9776aba0af56a92bf8e55315f44f3a9282
HazemSayad <94578938+HazemSayad@users.noreply.github.com>
2022-07-16 19:49:04 +0300

    added circular indicator

--- lib/views/homepage_view.dart
+++ lib/views/homepage_view.dart
@@ -9,7 +9,12 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Home Page")),
-      body: const Center(child: Text("Home Page Body")),
+      body: Column(
+        children: const [
+          Center(child: Text("Home Page Body")),
+          CircularProgressIndicator()
+        ],
+      ),
     );
   }
 }
