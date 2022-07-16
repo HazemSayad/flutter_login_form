bc1cfef3ea42fac55b9d6c52726bac064e8461e4
HazemSayad <94578938+HazemSayad@users.noreply.github.com>
2022-07-16 19:31:57 +0300

    added named routes to main

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_login_form/views/firsttime_view.dart';
 import 'package:flutter_login_form/views/homepage_view.dart';
+import 'package:flutter_login_form/views/login_view.dart';
+import 'package:flutter_login_form/views/signup_view.dart';
 
 void main() {
   bool isFirstTime = true;
 
   runApp(MaterialApp(
     title: "Flutter App",
+    routes: {
+      "/login/": (context) {
+        return const LoginView();
+      },
+      "/signup/": (context) {
+        return const SignupView();
+      }
+    },
     theme: ThemeData(primarySwatch: Colors.deepPurple),
     home: isFirstTime ? const FirstTimePage() : const HomePage(),
   ));
