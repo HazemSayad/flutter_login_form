1aef93b11b293888ffda0b1fc9e760894e204e51
HazemSayad <94578938+HazemSayad@users.noreply.github.com>
2022-07-16 19:28:46 +0300

    imporved main

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_login_form/views/login_view.dart';
-import 'package:flutter_login_form/views/signup_view.dart';
+import 'package:flutter_login_form/views/firsttime_view.dart';
+import 'package:flutter_login_form/views/homepage_view.dart';
 
 void main() {
+  bool isFirstTime = true;
+
   runApp(MaterialApp(
-    title: "Login Form",
+    title: "Welcome!",
     theme: ThemeData(primarySwatch: Colors.deepPurple),
-    home: const SignupView(),
+    home: isFirstTime ? const FirstTimePage() : const HomePage(),
   ));
 }
